fix minus sign problem

dictionary_learning.py
@@ -147,54 +147,7 @@ def signal_filter(sg, fr, freq=1/3, order=3, mode='high'):
     b, a = signal.butter(order, normFreq, mode)
     sg = np.single(signal.filtfilt(b, a, sg, padtype='odd', padlen=3 * (max(len(b), len(a)) - 1)))
     return sg
-#%%
-def combine_datasets(fnames, num_frames, x_shifts=[3,-3], y_shifts=[3,-3], weights=None, shape=(15,15)):
-    mm = 0
-    ephs = []
-    times_e = []
-    times_v = []
-    volt = []
-    spatial = []
-    if weights is None:
-        weights = [1/len(fnames)]*len(fnames)
-    for name,x_shift, y_shift, weight in zip(fnames,x_shifts,y_shifts, weights):
-        new_mov = cm.load(name)
-        dims = new_mov.shape[1:]
-        hh = nf_read_roi_zip((name[:-7] + '_ROI.zip'), dims=dims)
-        hh = hh.astype(np.float32)
-        new_hh = cm.movie(hh)
-        
-        if new_mov.shape[1] != shape[0]:
-            new_mov = new_mov.resize(shape[0]/new_mov.shape[1],shape[1]/new_mov.shape[2],1)
-            new_hh = new_hh.resize(shape[0]/hh.shape[1],shape[1]/hh.shape[2], 1)
-            
-        mm += np.roll(new_mov[:num_frames]*weight, (x_shift, y_shift), axis=(1,2))
-        new_hh = np.roll(new_hh, (x_shift, y_shift), axis=(1,2))
-        spatial.append(new_hh)   
-    
-        name_traces = '/'.join(name.split('/')[:-2] + ['data_new', name.split('/')[-1][:-7]+'_output.npz'])
-        #%
-        try:
-            with np.load(name_traces, allow_pickle=True) as ld:
-                dict1 = dict(ld)
-                time_v = dict1['v_t'] - dict1['v_t'][0]
-                time_e = dict1['e_t'] - dict1['e_t'][0]
-                time_v = time_v[:num_frames]
-                eph = normalize(dict1['e_sg'][time_e<time_v[-1]])
-                time_e = time_e[time_e<time_v[-1]]            
-                time_e /= np.max(time_e)
-                time_v /= np.max(time_v)
-                trep = normalize(dict1['v_sg'][:num_frames])
-                times_e.append(time_e)
-                times_v.append(time_v)
-                ephs.append(eph)
-                volt.append(trep)
-        except:
-            volt, ephs, times_v, times_e = [None]*4
-            
-        #%
-        
-    return mm, volt, ephs, times_v, times_e, spatial
+
 #%%
 #c, dview, n_processes = cm.cluster.setup_cluster(
 #        backend='local', n_processes=None, single_thread=False)

voltanon/nmf_nnls.py
@@ -81,7 +81,7 @@ H = np.vstack(H_tot)
 W = np.hstack(W_tot)
 
 #%% Use hals to optimize masks
-update_bg = True
+update_bg = False
 y_input = np.maximum(y_filt[:num_frames_init], 0)
 y_input = cm.movie(to_3D(y_input, shape=(num_frames_init,30,30), order='F')).transpose([1,2,0])
 
@@ -94,7 +94,7 @@ for i in range(2):
 #%% Use nnls to extract signal for neurons
 fe = slice(0,None)
 if update_bg:
-    trace_all = np.array([nnls(np.hstack((H_new, b)),yy)[0] for yy in (y_filt-y_filt.min())[fe]]) 
+    trace_all = np.array([nnls(np.hstack((H_new, b)),yy)[0] for yy in (-y)[fe]]) 
 else:
     trace_all = np.array([nnls(H_new,yy)[0] for yy in (-y)[fe]]) 
 
@@ -102,10 +102,9 @@ trace_all = signal_filter(trace_all.T,freq = 1/3, fr=frate).T
 trace_all = trace_all - np.median(trace_all, 0)[np.newaxis, :]
 
 plt.plot(trace_all[:, :])
-trace_all = trace_all.T
+trace_all = -trace_all.T
 
 trace_all = trace_all[:2]
-plt.plot(trace_all.T)
 
 #%% Extract spikes and compute F1 score
 v_sg = []