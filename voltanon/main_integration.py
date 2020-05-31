33b93edb5c421b4f346ccce556000722751acb32
agiovann <andrea.giovannucci@gmail.com>
2020-05-30 21:47:55 -0400

    main_integration working in two NNLS modes

--- voltanon/main_integration.py
+++ voltanon/main_integration.py
@@ -14,17 +14,19 @@ Use hals algorithm to refine spatial components extracted by rank-1 nmf.
 Use nnls and sao for further processing.
 @author: @agiovann and @caichangjia
 """
+#%%
+from caiman_functions import signal_filter, to_3D, to_2D
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 from scipy.optimize import nnls    
 from signal_analysis_online import SignalAnalysisOnline
 from sklearn.decomposition import NMF
-from caiman_functions import signal_filter, to_3D, to_2D
 from metrics import metric
 from nmf_support import hals, select_masks, normalize
 from skimage.io import imread
 import h5py
+
 #%% files for processing
 base_folder = ['/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/video_small_region/',
                '/home/nel/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/video_small_region/',
@@ -96,48 +98,56 @@ if do_plot:
     for i in range(2):
         plt.figure();plt.imshow(H_new[:,i].reshape(mov.shape[1:], order='F'));plt.colorbar()
     
-#%% Use nnls to extract signal for neurons
-fe = slice(0,None)
-if update_bg:
-    trace_nnls = np.array([nnls(np.hstack((H_new, b)),yy)[0] for yy in (-y)[fe]]) 
-else:
-    trace_nnls = np.array([nnls(H_new,yy)[0] for yy in (-y)[fe]]) 
 
-trace_nnls = signal_filter(trace_nnls.T,freq = 1/3, fr=frate).T
-trace_nnls -= np.median(trace_nnls, 0)[np.newaxis, :]
-trace_nnls = -trace_nnls.T
-trace_nnls = trace_nnls[:2]
-if do_plot:
-    plt.figure()
-    plt.plot(trace_nnls.T) 
 #%%
-mov_in = mov.transpose([1,2,0])
-Ab = H_new.astype(np.float32)
-template = np.median(mov_in, axis=-1)
-mc0 = mov_in[:,:,0:1][None, :]
-b =  to_2D(mov).T[:, 0]
-x0 = nnls(Ab,b)[0][:,None]
-AtA = Ab.T@Ab
-Atb = Ab.T@b
-n_AtA = np.linalg.norm(AtA, ord='fro') #Frob. normalization
-theta_2 = (Atb/n_AtA)[:, None].astype(np.float32)
+use_GPU = False
+if use_GPU: 
+    mov_in = mov.transpose([1,2,0])
+    Ab = H_new.astype(np.float32)
+    template = np.median(mov_in, axis=-1)
+    mc0 = mov_in[:,:,0:1][None, :]
+    b =  to_2D(mov).T[:, 0]
+    x0 = nnls(Ab,b)[0][:,None]
+    AtA = Ab.T@Ab
+    Atb = Ab.T@b
+    n_AtA = np.linalg.norm(AtA, ord='fro') #Frob. normalization
+    theta_2 = (Atb/n_AtA)[:, None].astype(np.float32)
+    #%%
+    from pipeline_gpu import Pipeline, get_model
+    model = get_model(template, Ab, 30)
+    model.compile(optimizer='rmsprop', loss='mse')
+    spike_extractor = Pipeline(model, x0[None, :], x0[None, :], mc0, theta_2, mov_in[:,:,:100000])
+    traces_viola = spike_extractor.get_spikes(100000)
+    #%%
+    traces_viola = np.array(traces_viola).squeeze().T
+    traces_viola = signal_filter(traces_viola,freq = 1/3, fr=frate).T
+    traces_viola -= np.median(traces_viola, 0)[np.newaxis, :]
+    traces_viola = -traces_viola.T
+    trace_all = np.hstack([traces_viola,np.zeros((traces_viola.shape[0],1))])
+
+else:
+    #%% Use nnls to extract signal for neurons
+    fe = slice(0,None)
+    if update_bg:
+        trace_nnls = np.array([nnls(np.hstack((H_new, b)),yy)[0] for yy in (-y)[fe]])
+    else:
+        trace_nnls = np.array([nnls(H_new,yy)[0] for yy in (-y)[fe]])
+    
+    trace_nnls = signal_filter(trace_nnls.T,freq = 1/3, fr=frate).T
+    trace_nnls -= np.median(trace_nnls, 0)[np.newaxis, :]
+    trace_nnls = -trace_nnls.T
+    if do_plot:
+        plt.figure()
+        plt.plot(trace_nnls.T) 
+    trace_all = trace_nnls
 #%%
-from pipeline_gpu import Pipeline, get_model
-model = get_model(template, Ab, 30)
-model.compile(optimizer='rmsprop', loss='mse')
-spike_extractor = Pipeline(model, x0[None, :], x0[None, :], mc0, theta_2, mov_in[:,:,:100000])
-traces_viola = spike_extractor.get_spikes(100000)
+# idxv = 0
+# plt.plot(np.hstack([traces_viola[idxv],0])-trace_nnls[idxv])
+# plt.plot(np.hstack([traces_viola[idxv],0])-trace_nnls[idxv])
+# #%%
+# plt.plot(trace_nnls[idxv])
+# plt.plot(traces_viola[idxv])
 #%%
-traces_viola = np.array(traces_viola).squeeze().T
-traces_viola = signal_filter(traces_viola,freq = 1/3, fr=frate).T
-traces_viola -= np.median(traces_viola, 0)[np.newaxis, :]
-traces_viola = -traces_viola.T
-traces_viola = traces_viola[:2]
-#%%
-plt.plot(np.concatenate([traces_viola[0],0])-trace_nnls[0])
-# plt.plot(trace_nnls[0])
-#%%
-trace_all = traces_viola
 #%% Extract spikes and compute F1 score
 v_sg = []
 all_f1_scores = []
@@ -149,7 +159,7 @@ for idx, k in enumerate(list(file_set)):
     sao = SignalAnalysisOnline(thresh_STD=4)
     #sao.fit(trr_postf[:20000], len())
     #trace=dict1['v_sg'][np.newaxis, :]
-    sao.fit(trace[:, :20000], num_frames=100000)
+    sao.fit(trace[:, :20000], num_frames=100000, frate=frate)
     for n in range(20000, trace.shape[1]):
         sao.fit_next(trace[:, n: n+1], n)
     sao.compute_SNR()
@@ -193,9 +203,10 @@ plt.plot(dict1['v_t'][:show_frames], t4[:show_frames]+0.5, label='gt2')
 #plt.plot(dict1['e_t'], t4-3, label='ele')
 #plt.vlines(dict1['e_sp'], -3, -2.5, color='black')
 plt.legend()
+  
 #%%   
 print(f'average_F1:{np.mean([np.nanmean(fsc) for fsc in all_f1_scores])}')
 print(f'F1:{np.array([np.nanmean(fsc) for fsc in all_f1_scores]).round(2)}')
 print(f'prec:{np.array([np.nanmean(fsc) for fsc in all_prec]).round(2)}'); 
-print(f'rec:{np.array([np.nanmean(f|sc) for fsc in all_rec]).round(2)}')
-print(f'snr:{np.array(all_snr).round(3)}')
+print(f'rec:{np.array([np.nanmean(fsc) for fsc in all_rec]).round(2)}')
+plt.pause(15)
