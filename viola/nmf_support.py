ce279b521db02ad3b062c7192e3a95982a2ca2b4
Andrea Giovannucci <agiovan@MBPro-M1.local>
2021-01-09 18:06:38 -0500

    attempt to fix zerba with halswithspikes

--- viola/nmf_support.py
+++ viola/nmf_support.py
@@ -15,7 +15,8 @@ import scipy.sparse as spr
 from sklearn.decomposition import NMF
 from .caiman_functions import to_3D, to_2D
 from .spikepursuit import denoise_spikes
-
+from viola.caiman_functions import signal_filter
+import scipy
 #%%
 def mode_robust_fast(inputData, axis=None):
     """
@@ -67,7 +68,7 @@ def _hsm(data):
 
         return _hsm(data[j:j + N])
 #%%
-def hals(Y, A, C, b, f, bSiz=3, maxIter=5, update_bg=True, use_spikes=False):
+def hals(Y, A, C, b, f, bSiz=3, maxIter=5, update_bg=True, use_spikes=False, frate=0):
     """ Hierarchical alternating least square method for solving NMF problem
     Y = A*C + b*f
     Args:
@@ -117,7 +118,7 @@ def hals(Y, A, C, b, f, bSiz=3, maxIter=5, update_bg=True, use_spikes=False):
         for _ in range(iters):
             for m in range(len(U)):  # neurons and background
                 C[m] = np.clip(C[m] + (U[m] - V[m].dot(C)) /
-                               V[m, m], 0, np.inf)
+                               V[m, m], -np.inf, np.inf)
         return C
     def HALS4shape(Yr, A, C, iters=2):
         U = C.dot(Yr.T)
@@ -134,9 +135,9 @@ def hals(Y, A, C, b, f, bSiz=3, maxIter=5, update_bg=True, use_spikes=False):
         return A
     Ab = np.c_[A, b]
     Cf = np.r_[C, f.reshape(nb, -1)]
-    for _ in range(maxIter):
-        Cf = HALS4activity(np.reshape(
-            Y, (np.prod(dims), T), order='F'), Ab, Cf)
+    # Ab = HALS4shape(np.reshape(Y, (np.prod(dims), T), order='F'), Ab, Cf)
+    for _ in range(maxIter):    
+        Cf = HALS4activity(np.reshape(Y, (np.prod(dims), T), order='F'), Ab, Cf)
         Cf_processed = Cf.copy()
 
         if not update_bg:
@@ -145,19 +146,30 @@ def hals(Y, A, C, b, f, bSiz=3, maxIter=5, update_bg=True, use_spikes=False):
         if use_spikes:
             for i in range(Cf.shape[0]):
                 if i < Cf.shape[0] - nb: 
-                    tr = Cf[i].copy()
-                    bl = mode_robust_fast(tr)
-                    tr -= bl
-                    tr = -tr
-                    shrinkage = 1#np.max(Cf[i]) / np.max(Cf_processed[i])
+                    orig_movie = False
+                    if orig_movie:
+                        bl = scipy.ndimage.percentile_filter(-Cf[i], 20, size=50)
+                        tr = -Cf[i] - bl
+                        # bl = mode_robust_fast(tr)
+                        # tr -= bl
+
+                    else:
+                        bl = scipy.ndimage.percentile_filter(Cf[i], 20, size=50)
+                        tr = Cf[i] - bl
+                    import pdb
+                    # pdb.set_trace()
+                    shrinkage = 1#np.max(Cf[i]) / np.max`(Cf_processed[i])
                     _, _, Cf_processed[i], _, _, _ = denoise_spikes(tr, window_length=3, clip=0, 
-                                      threshold=3.5, threshold_method='simple', do_plot=False)
+                                      threshold=2.0, threshold_method='simple', do_plot=False)
                     Cf_processed[i] = Cf_processed[i] * shrinkage
-                    Cf_processed[i] = -Cf_processed[i] + bl            
+                    if orig_movie:
+                        Cf_processed[i] = -Cf_processed[i] - bl    
+                    else:
+                        Cf_processed[i] = Cf_processed[i] + bl  
         Cf = Cf_processed
         Ab = HALS4shape(np.reshape(Y, (np.prod(dims), T), order='F'), Ab, Cf)
         # for i in range(Ab.shape[1]):
-        #     plt.figure();plt.imshow(Ab[:, i].reshape(Y.shape[0:2], order='F'));plt.colorbar()
+        #     plt.figure();plt.imshow(Ab[:, i].reshape((30,30), order='F'));plt.colorbar()
         
     return Ab[:, :-nb], Cf[:-nb], Ab[:, -nb:], Cf[-nb:].reshape(nb, -1)
 
